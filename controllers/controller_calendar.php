99f53a03d530a8b250a70ad696b7bcbdf8f270dd
Esteban <estebanfr20100@gmail.com>
2021-09-11 03:34:21 -0500

    Segunda

--- controllers/controller_calendar.php
+++ controllers/controller_calendar.php
@@ -1,7 +1,15 @@
 <?php
+include_once('connection.php');
+include_once('models/model_calendar.php');
 
-class ControllerCaldendar{
+class ControllerCalendar{
 
+//====================================================================================================
+    public function view(){
+        //$calendarData = Agenda::consult();
+        include_once('views/calendar/table.php');
+    }
+//====================================================================================================
 }
 
 ?>
